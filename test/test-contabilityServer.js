1c54149d770e60b77b251de832be5097cce8a10e
Octavian Rotari <octavianrotari@gmail.com>
2016-02-02 10:13:12 +0100

    first commit

---
+++ test/test-contabilityServer.js
@@ -0,0 +1,21 @@
+describe('bills', function(){
+  it('should list all the bills on Get  /fatture', function(){
+
+  });
+
+  it('should list specfic the bill on Get /fatture/<id>', function(){
+
+  });
+
+  it('should add a signle bill on POST  /fatture', function(){
+
+  });
+
+  it('should update a signle bill on PUT /fatture/<id>', function(){
+
+  });
+
+  it('should delete a single bill on delete  /fatturei/<id>', function(){
+
+  });
+});
